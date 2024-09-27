058e3f4b17b1b382a84a0b1828bc7a63772bd654
randymcmillan <randymcmillan@protonmail.com>
2024-09-26 22:23:47 -0400

    webview-examples/examples/dialog.rs

--- webview-examples/examples/dialog.rs
+++ webview-examples/examples/dialog.rs
@@ -118,8 +118,75 @@ button:focus {
 }
 
 </style>
+
+
+
 </head>
 <body>
+
+<script src="https://unpkg.com/@isomorphic-git/lightning-fs"></script>
+<script src="https://unpkg.com/isomorphic-git@beta"></script>
+<script type="module">
+import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
+// Initialize isomorphic-git with a file system
+window.fs = new LightningFS('fs')
+// I prefer using the Promisified version honestly
+window.pfs = window.fs.promises
+
+window.dir = '/tutorial'
+console.log(dir);
+await pfs.mkdir(dir);
+// Behold - it is empty!
+await pfs.readdir(dir);
+
+await git.clone({
+  fs,
+  http,
+  dir,
+  corsProxy: 'https://cors.isomorphic-git.org',
+  url: 'https://github.com/isomorphic-git/isomorphic-git',
+  ref: 'main',
+  singleBranch: true,
+  depth: 10
+});
+
+// Now it should not be empty...
+await pfs.readdir(dir);
+
+await git.log({fs, dir})
+    await git.status({fs, dir, filepath: 'README.md'})
+    await pfs.writeFile(`${dir}/README.md`, 'Very short README', 'utf8')
+await git.status({fs, dir, filepath: 'README.md'})
+await git.add({fs, dir, filepath: 'README.md'})
+await git.status({fs, dir, filepath: 'README.md'})
+await pfs.writeFile(`${dir}/newfile.txt`, 'Hello World', 'utf8')
+await git.status({fs, dir, filepath: 'newfile.txt'})
+await git.add({fs, dir, filepath: 'newfile.txt'})
+await git.status({fs, dir, filepath: 'newfile.txt'})
+let sha = await git.commit({
+  fs,
+  dir,
+  message: 'Delete package.json and overwrite README.',
+  author: {
+    name: 'Mr. Test',
+    email: 'mrtest@example.com'
+  }
+})
+
+console.log(sha)
+    let commits = await git.log({fs, dir, depth: 1})
+console.log(commits[0])
+
+
+var path = document.location.pathname;
+var dir = path.substring(path.indexOf('DIRLISTTEST/', 1),
+path.lastIndexOf('/'));
+document.write(dir);
+
+
+</script>
+
+
 <ul>
 <li>
     <button onclick="external.invoke('open')">Open</button>
@@ -140,6 +207,19 @@ button:focus {
     <button onclick="external.invoke('exit')">Exit</button>
 </li>
 </ul>
+
+git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs', corsProxy: 'https://cors.isomorphic-git.org' }).then(console.log)
+await git.clone({
+  fs,
+  http,
+  dir: '/tutorial',
+  corsProxy: 'https://cors.isomorphic-git.org',
+  url: 'https://github.com/isomorphic-git/isomorphic-git',
+  singleBranch: true,
+  depth: 1
+})
+console.log('done')
+
 </body>
 </html>
 "#;
